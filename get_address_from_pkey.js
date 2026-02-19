a217b383fdc52403ac85b31a78588a3c4267fccd
Victor Giron <vgiron@izertis.com>
2026-02-19 14:27:20 +0100

    cleanAndAddStuffs

--- get_address_from_pkey.js
+++ get_address_from_pkey.js
@@ -2,7 +2,7 @@
 const { ethers } = require("ethers");
 
 // Hardcodea aquí tu private key (con 0x)
-const PRIVATE_KEY = "f6da5c27819dd68d4e5a87f3480e14be6d4d843eb7906094468cc18159a974ba";
+const PRIVATE_KEY = "xxx";
 
 const wallet = new ethers.Wallet(PRIVATE_KEY);
 console.log("Address:", wallet.address);
